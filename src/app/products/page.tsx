f5973df56eeb88b069231bbc7ed9ceb2681ce05e
felipeneneu <feliperpg3@gmail.com>
2025-02-20 23:15:11 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div>
+      pages
+      <Button>a</Button>
+    </div>
+  );
+};
+
+export default ProductPage;
